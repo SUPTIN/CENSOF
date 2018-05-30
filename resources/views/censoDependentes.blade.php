64a7f0de7a2d66eb05307f53603b69290c3344eb
elyrio <lyrio.nas@gmail.com>
2018-05-30 12:36:23 -0300

    Ficha de dependnete.

---
+++ resources/views/censoDependentes.blade.php
@@ -0,0 +1,27 @@
+@extends('layout.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                      <div class="row">
+                        <h4>Censo Funcional - Dependentes</h4>
+                        <h6>Passo 4 de 4</h6>
+                      </div>
+
+                      <div class="row">
+                        <div class="col-sm-12">
+                          
+                          <a href="novoDependente"> Para adicionar novo Dependente Clique aqui</a>
+                        </div>
+                      </div>
+
+                </div>
+            </div>
+      </div>
+   </div>
+</div>
+@endsection
+
