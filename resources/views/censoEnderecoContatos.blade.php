2ffa22f4be2feb28800121612241834c6ebcbef9
elyrio <lyrio.nas@gmail.com>
2018-06-27 10:36:58 -0300

    Adicionando busca de endereço por cep.

--- resources/views/censoEnderecoContatos.blade.php
+++ resources/views/censoEnderecoContatos.blade.php
@@ -11,44 +11,44 @@
                         <h6>Passo 2 de 4</h6> 
                       </div>
 
+                      <form method="post" action="eC">
                       <div class="row">
-                        <div class="col-sm-10">
-                          Endereço Residencial: 
-                          <input class="form-control" name="enderecoResidencial" value="{{old('enderecoResidencial')}}"/>
+                        <div class="col-sm-6">
+                          CEP:
+                          <input class="form-control" id="cep" name="cep" value="{{old('cep')}}" onblur="pesquisaCep(this.value);"/>
                         </div>
-                        <div class="col-sm-2">
-                          Número:
-                          <input class="form-control" name="numero" value="{{old('numero')}}"/>
-                        </div>
-                      </div>
-
-                      <div class="row">
-                        <div class="col-sm-12">
-                          Complemento:
-                          <input class="form-control" name="complemento" value="{{old('complemento')}}"/>
+                        <div class="col-sm-6">
+                          Estado:
+                          <input class="form-control" id="estadoResidencia" name="estadoResidencia" value="{{old('estadoResidencia')}}"/>
                         </div>
                       </div>
 
                       <div class="row">
                         <div class="col-sm-6">
                           Cidade:
-                          <input class="form-control" name="cidade" value="{{old('cidade')}}"/>
+                          <input class="form-control" id="cidade" name="cidade" value="{{old('cidade')}}"/>
                         </div>
                         <div class="col-sm-6">
                           Bairro:
-                          <input class="form-control" name="bairro" value="{{old('bairro')}}"/>
-                        </div>
-                        
+                          <input class="form-control" id="bairro" name="bairro" value="{{old('bairro')}}"/>
+                        </div> 
                       </div>
 
                       <div class="row">
-                        <div class="col-sm-6">
-                          Estado:
-                          <input class="form-control" name="cep" value="{{old('cep')}}"/>
+                        <div class="col-sm-10">
+                          Endereço Residencial: 
+                          <input class="form-control" id="enderecoResidencial" name="enderecoResidencial" value="{{old('enderecoResidencial')}}"/>
                         </div>
-                        <div class="col-sm-6">
-                          CEP:
-                          <input class="form-control" name="estadoResidencia" value="{{old('estadoResidencia')}}"/>
+                        <div class="col-sm-2">
+                          Número:
+                          <input class="form-control" id="numero" name="numero" value="{{old('numero')}}"/>
+                        </div>
+                      </div>
+
+                      <div class="row">
+                        <div class="col-sm-12">
+                          Complemento:
+                          <input class="form-control" id="complemento" name="complemento" value="{{old('complemento')}}"/>
                         </div>
                       </div>
 
@@ -71,15 +71,66 @@
                         </div>
                       </div>
 
-                      <div class="row" align="center">
+                      <div class="row">
                         <div class="col-sm-12">
-                          <a href="documentacao"> Proximo passo</a>
+                          <br/>
                         </div>
                       </div>
+                      <div class="row" align="center">
+                        <div class="col-sm-12"> 
+                          <button class="btn btn-primary">Avançar</button>
+                        </div>
+                      </div>
+                    </form>
 
                 </div>
             </div>
       </div>
+
+      <script type="text/javascript">
+
+          function limpa_cep(){
+            document.getElementById('cidade').value = ("");
+            document.getElementById('bairro').value = ("");
+            document.getElementById('enderecoResidencial').value = ("");
+            document.getElementById('estadoResidencia').value = ("");
+          }
+
+          function meu_callback(conteudo){
+            if(!("erro" in conteudo)){
+              document.getElementById('cidade').value = (conteudo.localidade);
+              document.getElementById('bairro').value = (conteudo.bairro);
+              document.getElementById('enderecoResidencial').value = (conteudo.logradouro);
+              document.getElementById('estadoResidencia').value = (conteudo.uf);
+            }else{
+              limpa_cep();
+              alert("CEP não encontrado!");
+            }
+          }
+
+          function pesquisaCep(valor){
+            var cep = valor.replace(/\D/g, '');
+            if(cep != ""){
+              var validaCep = /^[0-9]{8}$/;
+              if(validaCep.test(cep)){
+                document.getElementById('cidade').value = ("...");
+                document.getElementById('bairro').value = ("...");
+                document.getElementById('enderecoResidencial').value = ("...");
+                document.getElementById('estadoResidencia').value = ("...");
+                var script = document.createElement('script');
+                script.src = 'https://viacep.com.br/ws/'+ cep +'/json/?callback=meu_callback';
+                document.body.appendChild(script);
+              }else{
+                limpa_cep();
+                alert("Formato de CEP inválido!");
+              }
+            }else{
+              limpa_cep();
+            }
+          };
+   
+      </script>
+
    </div>
 </div>
 @endsection
