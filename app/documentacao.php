1c6ba4db37cd47e443fcb0b5f65c9f5a3bc11906
elyrio <lyrio.nas@gmail.com>
2018-06-29 10:13:45 -0300

    Corrigindo nome de campo.

--- app/documentacao.php
+++ app/documentacao.php
@@ -8,5 +8,5 @@ class documentacao extends Model
 {
     protected $table = 'documentacao';
     protected $primaryKey = 'idDocumentacao';
-    protected $fillable = ['idDadosBase', 'cpf', 'rg', 'orgaoEmissorRG', 'ufRg', 'dataEmissaoRg', 'ctps', 'serie', 'ufCtps', 'dataEmissaoCtps', 'pisPasep', 'dataCadPisPasep', 'tituloEleitor', 'zona', 'secao', 'dataEmissaoTituloEleitor', 'ufVotacao', 'cidadeVotacao', 'certMilitar', 'certMilitarSituacao', 'dataCertMilitar', 'tipoCertMilitar', 'ufCertMilitar', 'numCNH', 'registroCNH', 'categoriaCNH', 'dataEmissaoCNH', 'ufCNH', 'primeiraHabilitacao', 'conselhoProfissional', 'numConselhoProf', 'dataEmissaoConselhoProf', 'dataValidadeConselhoProf'];
+    protected $fillable = ['idDadosBase', 'cpf', 'rg', 'orgaoEmissorRG', 'ufRg', 'dataEmissaoRg', 'ctps', 'serie', 'ufCtps', 'dataEmissaoCtps', 'pisPasep', 'dataCadPisPasep', 'tituloEleitor', 'zona', 'secao', 'dataEmissaoTituloEleitor', 'ufVotacao', 'cidadeVotacao', 'certMilitar', 'certMilitarSituacao', 'dataCertMilitar', 'tipoCertMilitar', 'ufCertMilitar', 'numCNH', 'registroCNH', 'categoriaCNH', 'dataEmissaoCNH', 'dataValideCNH', 'ufCNH', 'primeiraHabilitacao', 'conselhoProfissional', 'numConselhoProf', 'dataEmissaoConselhoProf', 'dataValidadeConselhoProf'];
 }
