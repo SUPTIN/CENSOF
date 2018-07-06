ea57c53c564490060999385deddefce5fcbdc25a
elyrio <lyrio.nas@gmail.com>
2018-07-06 09:48:31 -0300

    Criando dados dos sistema.

---
+++ resources/views/censoInformacaoSistema.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                      <div class="row">
+                        <h4>Censo Funcional - Dados Sistema </h4>
+                      </div>
+   </div>
+</div>
+@endsection
+
