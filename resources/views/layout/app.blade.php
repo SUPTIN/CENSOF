e55d0193fdf28c25776fec8d12e83c9208904888
elyrio <lyrio.nas@gmail.com>
2018-09-21 16:44:58 -0300

    corrigindo ordem de menu.

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -81,6 +81,12 @@
 
                                 <ul class="dropdown-menu">
                                     <li>
+                                        <a href="{{ url('trocaSenha') }}">
+                                            Troca Senha
+                                        </a>
+                                    </li>
+
+                                    <li>
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
@@ -90,12 +96,6 @@
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                             {{ csrf_field() }}
                                         </form>
-                                    </li>
-
-                                    <li>
-                                        <a href="{{ url('trocaSenha') }}">
-                                            Troca Senha
-                                        </a>
                                     </li>
                                 </ul>
 
