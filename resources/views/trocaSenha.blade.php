b8427282c993b12401cd81e744f8be6ab359865f
elyrio <lyrio.nas@gmail.com>
2018-09-21 13:55:23 -0300

    Iniciando form troca senha.

---
+++ resources/views/trocaSenha.blade.php
@@ -0,0 +1,35 @@
+@extends('layout.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                      <div class="row">
+                        <h4>Censo Funcional - Troca Senha</h4>
+                      </div>
+
+                      <div class="row">
+                        <div class="col-sm-12">
+                          <label style="font-size:10px;color:red;"> * Os campos são obrigatórios</label>
+                        </div>
+                      </div>
+
+                      <div class="row">
+                        <div class="col-sm-12">
+                          
+                        </div>
+                      </div>
+
+                      <div class="row">
+                        <div class="col-sm-12">
+                          <br/>
+                        </div>
+                      </div>
+            </div>
+      </div>
+   </div>
+</div>
+@endsection
+
